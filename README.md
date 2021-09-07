8f7a015c17ecb870f7d08b5bccc3f12648a0f344
DolosKurosu <84930922+DolosKurosu@users.noreply.github.com>
2021-09-07 14:32:17 -0400

    Added code to make a items_metadata.txt file

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Note: The itempools.xml was taken from Item Pools Rework mod to use for testing.
 ## How To Use
 1) Open this project folder on VSCode.
 2) Replace the itempools.xml and/or item-metadata.xml in the `assets` folder if generating recipes for another mod.
-3) Go to index.js in the `src` folder, and press `CTRL` + `SHIFT` + `grave` to open up a terminal.
+3) Go to itemdatatoluatable.js in the `src` folder, and press `CTRL` + `SHIFT` + `grave` to open up a terminal.
 4) Type `npm install` to install all dependencies for the project.
 5) Type `node src/itemdatatoluatable.js` in the console and press `ENTER`.
--- src/itemdatatoluatable.js
+++ src/itemdatatoluatable.js
@@ -3,12 +3,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 const XmlParser_1 = require("./XmlParser");
 let pools = XmlParser_1.XmlParser.loadPools(fs.readFileSync('assets/itempools.xml', 'utf8'));
+let meta = XmlParser_1.XmlParser.loadMeta(fs.readFileSync('assets/items_metadata.xml', 'utf8'));
 
-let list = "itempools ={";
+let list = "local itempools ={";
 for (let k = 0; k <= 30; k++) {
     list += ((JSON.stringify(pools[k].items.sort(function(x,y) { return x.id - y.id})).replace("[",`\n{`)).replace("]", "}")).replace("}}", `},}, -- ${pools[k].name}`);
     if (k == 30) {
         list += "\n}";
     }
 }
-fs.writeFileSync(`src/itempooldata.txt`, list);
+fs.writeFileSync(`src/itempools.txt`, list);
+
+let list2 = "EID.itemWeightsLookup= {\n";
+let count = 0;
+for (let k = 1; k <= 729; k++) {
+    if (meta.get(k)) {
+        count++;
+        list2 += `[${k}]=${JSON.stringify(meta.get(k))},`;
+        if (count % 23 == 0) {
+            list2 += "\n";
+        }
+        else {
+            list2 += " ";
+        }
+        if (k == 729) {
+            list2 += "\n}";
+        }
+    }
+}
+fs.writeFileSync(`src/items_metadata.txt`, list2);
